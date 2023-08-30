a89f20bc0c13bf24a660ce0efa76758b52832a18
Joe McCann <joe.mccann@mailbox.org>
2023-08-30 02:21:56 -0400

    reformatted to use 2 spaces per tab

--- java/bird-watcher/src/main/java/BirdWatcher.java
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -1,48 +1,47 @@
 
 class BirdWatcher {
-    private final int[] birdsPerDay;
-    private final int todayIndex;
+  private final int[] birdsPerDay;
+  private final int todayIndex;
 
+  public BirdWatcher(int[] birdsPerDay) {
+    this.birdsPerDay = birdsPerDay.clone();
+    this.todayIndex = birdsPerDay.length - 1;
+  }
 
-    public BirdWatcher(int[] birdsPerDay) {
-        this.birdsPerDay = birdsPerDay.clone();
-        this.todayIndex = birdsPerDay.length - 1;
-    }
+  public int[] getLastWeek() {
+    return this.birdsPerDay;
+  }
 
-    public int[] getLastWeek() {
-        return this.birdsPerDay;
-    }
+  public int getToday() {
+    return this.birdsPerDay[todayIndex];
+  }
 
-    public int getToday() {
-        return this.birdsPerDay[todayIndex];
-    }
+  public void incrementTodaysCount() {
+    this.birdsPerDay[todayIndex] += 1;
+  }
 
-    public void incrementTodaysCount() {
-        this.birdsPerDay[todayIndex] += 1;
-    }
+  public boolean hasDayWithoutBirds() {
+    for (int birdCount : this.birdsPerDay)
+      if (birdCount == 0)
+        return true;
 
-    public boolean hasDayWithoutBirds() {
-        for (int birdCount : this.birdsPerDay)
-          if (birdCount == 0)
-            return true;
+    return false;
+  }
 
-        return false;
-    }
+  public int getCountForFirstDays(int numberOfDays) {
+    int dailyCount = 0;
+    for (int dayIndex = 0; (dayIndex < numberOfDays) && (dayIndex < 7); dayIndex++)
+      dailyCount += this.birdsPerDay[dayIndex];
 
-    public int getCountForFirstDays(int numberOfDays) {
-        int dailyCount = 0;
-        for (int dayIndex = 0; (dayIndex < numberOfDays) && (dayIndex < 7); dayIndex++)
-            dailyCount += this.birdsPerDay[dayIndex];
-        
-        return dailyCount;
-    }
+    return dailyCount;
+  }
 
-    public int getBusyDays() {
-        int numberOfBusyDays = 0;
-        for (int birdCount : this.birdsPerDay)
-          if (birdCount > 4)
-            numberOfBusyDays += 1;
-        
-        return numberOfBusyDays;
-    }
+  public int getBusyDays() {
+    int numberOfBusyDays = 0;
+    for (int birdCount : this.birdsPerDay)
+      if (birdCount > 4)
+        numberOfBusyDays += 1;
+
+    return numberOfBusyDays;
+  }
 }
