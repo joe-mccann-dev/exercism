39371d8ad871414d3372d92de5c0d5227ee5cb2f
Joe McCann <joe.mccann@mailbox.org>
2023-09-01 00:54:55 -0400

    Complete Scrabble with tests passing.

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,32 @@
 class Scrabble {
 
-    Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+  int score = 0;
 
-    int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+  Scrabble(String word) {
+    setScore(word);
+  }
+
+  void setScore(String word) {
+    for (char ch : word.toCharArray())
+      this.score += getLetterScore(ch);
+  }
+
+  int getScore() {
+    return this.score;
+  }
+
+  int getLetterScore(char letter) {
+    letter = Character.toLowerCase(letter);
+    return switch (letter) {
+      case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't' -> 1;
+      case 'd', 'g' -> 2;
+      case 'b', 'c', 'm', 'p' -> 3;
+      case 'f', 'h', 'v', 'w', 'y' -> 4;
+      case 'k' -> 5;
+      case 'j', 'x' -> 8;
+      case 'q', 'z' -> 10;
+      default -> 0;
+    };
+  }
 
 }
