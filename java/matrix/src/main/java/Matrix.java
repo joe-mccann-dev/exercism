746d081a5db33654553f28b20012a26641f49c56
Joe McCann <joe.mccann@mailbox.org>
2023-09-07 01:00:27 -0400

    Clean up code. Remove unnecessary comments.

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -5,21 +5,19 @@ class Matrix {
     int matrixWidth;
     int[][] matrix;
 
-    // initially split matrixAsString into an array of strings, 
-    // each containing a string representation of a row of integers
-    // Then, split that row. Proceed to populate array with the split row's values.
     Matrix(String matrixAsString) {
+        // split into string array like => ["1 2 3", "4 5 6"];
         String[] numbers = matrixAsString.split("\n");
 
-        this.matrixLength = matrixAsString.split("\n").length;
-        // first row will have the same width as all others
-        this.matrixWidth = numbers[0].split(" ").length;
-
+        this.matrixLength = numbers.length;
+        this.matrixWidth = numbers[0].split("\\s").length;
         this.matrix = new int[matrixLength][matrixWidth];
         
         for (int rowIndex = 0; rowIndex < matrixLength; rowIndex++) {
-            String[] row = numbers[rowIndex].split(" ");
+            // split each row into its own row => ["1", "2", "3"];
+            String[] row = numbers[rowIndex].split("\\s");
             for (int columnIndex = 0; columnIndex < row.length; columnIndex++) {
+                // populate matrix with parsed integer values
                 int value = Integer.parseInt(row[columnIndex]);
                 matrix[rowIndex][columnIndex] = value;
             }
