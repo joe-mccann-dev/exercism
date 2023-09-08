435d945541c2a0a328d9154778384b66fe303e11
Joe McCann <joe.mccann@mailbox.org>
2023-09-08 00:48:57 -0400

    Add comments.

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -3,6 +3,10 @@ import java.util.List;
 
 class Flattener {
 
+    // calls private overloaded method
+    // enables modifying result thru second parameter
+    // private flatten method then calls itself recursively
+    // when a list is encountered
     public <T> List<T> flatten(List<T> list) {
         List<T> result = new ArrayList<>();
         flatten(list, result);
@@ -10,6 +14,7 @@ class Flattener {
         return result;
     }
 
+    // only need to worry about List<T>
     @SuppressWarnings("unchecked")
     private <T> void flatten(List<T> list, List<T> result) {
         for (T listItem : list) {
