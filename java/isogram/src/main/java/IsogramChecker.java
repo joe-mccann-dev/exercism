61266260d9062735985fdfcfa635383679208b31
Joe McCann <joe.mccann@mailbox.org>
2023-09-13 20:07:54 -0400

    Complete first refactor of IsogramChecker.java

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -4,37 +4,27 @@ import java.util.Map;
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        // create an empty HashMap<Character, Integer> with char being the letter and
-        // Integer being num times it occurs
-        // convert phrase toLowerCase()
-        // for each letter character of phrase put to hash map
-        // iterate thru hash map, return false if a count is greater than 1
-
         Map<Character, Integer> letterMap = new HashMap<>();
-        phrase = phrase.toLowerCase();
 
-        for (int i = 0; i < phrase.length(); i++) {
-            char ch = phrase.charAt(i);
-            if (Character.isLetter(ch))
-                letterMap.put(ch, 0);        
-        }
+        char[] letters = phrase.chars()
+                .filter(Character::isLetter)
+                .mapToObj(c -> Character.toLowerCase((char) c))
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString()
+                .toCharArray();
 
-        for (int i = 0; i < phrase.length(); i++) {
-            char ch = phrase.charAt(i);
-            int letterCount;
-            if (Character.isLetter(ch)) {
-                letterCount = letterMap.get(ch);
-                letterMap.put(ch, letterCount + 1);
-            }
-        }
+        for (Character ch : letters)
+            letterMap.put(ch, 0);
 
-        for (Integer letterCount : letterMap.values()) {
-            if (letterCount > 1)
+        for (Character ch : letters) {
+            int letterCount = letterMap.get(ch);
+            letterMap.put(ch, letterCount + 1);
+            int updatedCount = letterMap.get(ch);
+            if (updatedCount > 1)
                 return false;
         }
 
         return true;
-
     }
 
 }
