69f89e67f3d82878095ba2faa3c28f68c298deae
Joe McCann <joe.mccann@mailbox.org>
2023-09-14 12:07:19 -0400

    Complete second refactor of Anagram using streams.

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,8 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 class Anagram {
 
@@ -15,66 +14,54 @@ class Anagram {
     }
 
     public List<String> match(List<String> candidates) {
-        List<String> result = new ArrayList<>();
-        for (String candidate : candidates) {
-            if (isAnagram(candidate))
-                result.add(candidate);
-        }
-
-        return result;
-    }
-
-    private boolean isAnagram(String candidate) {
-        if (getWordToMatch().length() != candidate.length())
-            return false;
-
-        if (getWordToMatch().equalsIgnoreCase(candidate))
-            return false;
-
-        Map<Character, Integer> candidateLetterMap = getCandidateLetterMap(candidate);
-
-        for (Character ch : getWordToMatch().toCharArray()) {
-            int letterCount = getWordToMatchMap().get(ch);
-            int candidateLetterCount = Optional.ofNullable(candidateLetterMap.get(ch)).orElse(0);
-            if (letterCount != candidateLetterCount)
-                return false;
-        }
-
-        return true;
-    }
-
-    private Map<Character, Integer> getCandidateLetterMap(String candidate) {
-        Map<Character, Integer> currentWordMap = new HashMap<>();
-        char[] candidateChars = candidate.toLowerCase().toCharArray();
-
-        for (Character ch : candidateChars) {
-            currentWordMap.computeIfAbsent(ch, k -> 0);
-            currentWordMap.put(ch, currentWordMap.get(ch) + 1);
-        }
-
-        return currentWordMap;
-    }
-
-    private String getWordToMatch() {
-        return this.wordToMatch;
+        return candidates.stream().filter(candidate -> isAnagram(candidate)).toList();
     }
 
     private void setWordToMatch(String word) {
         this.wordToMatch = word;
     }
 
+    private void setWordToMatchMap() {
+        this.wordToMatchMap = getWordToMatch().chars()
+                .mapToObj(ch -> (char) ch)
+                .collect(Collectors.toMap(
+                        ch -> ch,
+                        ch -> 1,
+                        Integer::sum));
+    }
+
+    private boolean isAnagram(String candidate) {
+        if (wordsAreDifferentLengthOrIdentical(candidate))
+            return false;
+
+        var wordToMatchChars = getWordToMatch().chars().mapToObj(ch -> (char) ch);
+
+        return wordToMatchChars.allMatch(ch -> {
+            int letterCount = getWordToMatchMap().get(ch);
+            int candidateLetterCount = Optional.ofNullable(getCandidateMap(candidate).get(ch)).orElse(0);
+            return letterCount == candidateLetterCount;
+        });
+    }
+
+    private boolean wordsAreDifferentLengthOrIdentical(String candidate) {
+        return getWordToMatch().length() != candidate.length() ||
+                getWordToMatch().equalsIgnoreCase(candidate);
+    }
+
+    private String getWordToMatch() {
+        return this.wordToMatch;
+    }
+
     private Map<Character, Integer> getWordToMatchMap() {
         return this.wordToMatchMap;
     }
 
-    private void setWordToMatchMap() {
-        this.wordToMatchMap = new HashMap<>();
-
-        char[] wordChars = getWordToMatch().toCharArray();
-
-        for (Character ch : wordChars) {
-            this.wordToMatchMap.computeIfAbsent(ch, k -> 0);
-            this.wordToMatchMap.put(ch, getWordToMatchMap().get(ch) + 1);
-        }
+    private Map<Character, Integer> getCandidateMap(String candidate) {
+        return candidate.toLowerCase().chars()
+                .mapToObj(ch -> (char) ch)
+                .collect(Collectors.toMap(
+                        ch -> ch,
+                        ch -> 1,
+                        Integer::sum));
     }
 }
