a9eb06e81266dfd2fcf3cbe72b48505f7a2096b0
Joe McCann <joe.mccann@mailbox.org>
2023-09-16 01:55:28 -0400

    Add leftPad and centerPad string functions.

--- java/diamond/src/main/java/DiamondPrinter.java
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -3,17 +3,40 @@ import java.util.List;
 
 class DiamondPrinter {
 
+    final static int ASCII_UPPERCASE_A = 65;
+
     List<String> printToList(char letter) {
         List<String> result = new ArrayList<>();
-        int distanceFromA = (int) letter - (int) 'A';
-        char middleLetter = (char) (distanceFromA + (int) 'A');
+        int asciiLetter = (int) letter;
+        int distanceFromAToLetter = Math.abs((ASCII_UPPERCASE_A - asciiLetter));
+        int startToMiddle = (distanceFromAToLetter);
+        int diamondHeight = (startToMiddle * 2) + 1;
 
-        char oneBeforeMiddleLetter = (char) ((int) (middleLetter - 1));
-        result.add(String.valueOf(" " + (oneBeforeMiddleLetter)) + " ");
-        result.add(String.valueOf(middleLetter) + " " + String.valueOf(middleLetter));
-        result.add(String.valueOf(" " + (oneBeforeMiddleLetter)) + " ");
+        String centerPaddedA = centerPad(distanceFromAToLetter, "A");
+
+
+
+        result.add(centerPaddedA);
+
 
         return result;
+    }
+
+    private String leftPad(int distanceFromAToLetter, String letter) {
+        int leftWidth = distanceFromAToLetter + 1;
+        String leftPadded = String.format("%" + (leftWidth) + "s", "A");
+        
+        return leftPadded;
+    }
+
+    private String centerPad(int distanceFromAToLetter, String letter) {
+        int leftWidth = distanceFromAToLetter + 1;
+        int totalWidth = (leftWidth * 2) - 1;
+
+        String leftPadded = leftPad(distanceFromAToLetter, letter);
+        String centerPadded = String.format("%" + (-totalWidth) + "s", leftPadded);
+
+        return centerPadded;
     }
 
 }
